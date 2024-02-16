1d8b58676b6baa58908ab0aaabb0b9c385ceaa61
Clarence Joseph Dimafelix <clarencejoseph.dimafelix@gmail.com>
2024-02-16 15:04:15 +0800

    feat: login / logout

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Resources\AuthResource;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -33,4 +34,54 @@ class AuthController extends Controller
             ], 400);
         }
     }
+
+    public function login(Request $request)
+    {
+        $validateData = $request->validate([
+            'email' => 'email|required',
+            'password' => 'required'
+        ]);
+
+        // LOGIN SPA AUTH
+        if (!auth()->attempt($validateData)) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Invalid credentials'
+            ], 401);
+        }
+
+
+        $request->session()->regenerate();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Log in successful',
+            'user' => new AuthResource(Auth::user())
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+
+        Auth::guard('web')->logout();
+        $request->session()->invalidate();
+        // $request->session()->regenerateToken();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Logged out'
+        ]);
+    }
+
+    // public function test()
+    // {
+    //     return response()->json([
+    //         'success' => true,
+    //         'message' => 'You accessed this',
+    //     ]);
+
+    //     // Return the logged in user
+
+    //     // return new AuthResource(Auth::user());
+    // }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -22,3 +22,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 // AUTH ROUTES
 Route::post('/register', AuthController::class . '@register');
+Route::post('/login', AuthController::class . '@login');
+Route::post('/logout', AuthController::class . '@logout');
+
+// Route::post('/test', AuthController::class . '@test')->middleware('auth:sanctum');
