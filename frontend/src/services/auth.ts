bec3547e9cd669db1277ccabae498bdc4cef28df
Clarence Joseph Dimafelix <clarencejoseph.dimafelix@gmail.com>
2024-02-22 16:17:50 +0800

    feat: login user

--- frontend/src/services/auth.ts
+++ frontend/src/services/auth.ts
@@ -4,19 +4,39 @@ import { FieldValues } from "react-hook-form";
 import getCSRF from "./csrf";
 import { cookies } from "next/headers";
 
-// export async function login() {
-//   // Send the login request
-//   const response = await fetch("http://localhost:8000/api/login", {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//       Accept: "application/json",
-//       // Get the xsrf token from the cookie
-//       "x-xsrf-token": cookies().get("XSRF-TOKEN")?.value as string,
-//     },
-//     credentials: "include",
-//     body: JSON.stringify({}),
-//   });
+type TUser = {
+  id: number;
+  username: string;
+  email: string;
+  displayName?: string;
+  avatar?: string;
+};
 
-//   return response;
-// }
+function setTokenToCookies(token: string) {
+  cookies().set("token", token);
+}
+
+function setUserToCookies(user: TUser) {
+  cookies().set("userId", user.id.toString());
+}
+
+export async function login(data: FieldValues) {
+  const res = await fetch("http://localhost:8000/api/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  const resData = await res.json();
+
+  if (res.ok) {
+    setTokenToCookies(resData.token);
+    setUserToCookies(resData.user);
+    return { success: true, data: resData.user };
+  } else {
+    return resData;
+  }
+}
